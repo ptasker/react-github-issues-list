4da6b6919624a61ecb3358ec2dd3d8685bcac5ed
ptasker <tasker82@gmail.com>
2017-05-10 15:35:39 -0400

    Weather loading from Yahoo 🌤️

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Weather from './Weather';
+import WeatherInfo from './WeatherInfo';
 
-class App extends Component {
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+
+    this.loadWeather = this.loadWeather.bind(this);
+  };
+
+  loadWeather() {
+    return fetch('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22ottawa%2C%20on%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // let json = responseJson.results.forecast;
+        let json = responseJson.query.results.channel.item.forecast;
+        this.setState({ data: json });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +37,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Header title="What's up y'all?" />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
+        <div className="weather-box">
+          {
+            Object.keys( this.state.data ).map( key =>
+								<WeatherInfo key={key} index={key} details={this.state.data[ key ]} /> )
+          }
+        </div>
+        <Weather loadWeather={this.loadWeather} state={this.state} />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import Header from './Header';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+});
+
+it('renders header with props', () => {
+    const header = shallow(<Header title={'What\'s up doc?'} />);
+    expect(header.contains('What\'s up doc?')).toEqual(true);
 });
